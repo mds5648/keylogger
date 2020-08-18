b4c67553403f3c19f593b913b114632f29358546
mds5648 <mds5648@rit.edu>
2020-08-17 21:54:08 -0400

    Intial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Description:
+Language: Python 3.8
+Author: Matthew Stoffolano
+"""
+import keyboard
+
+def main():
+    recorded = keyboard.record('esc')
+
+
+if __name__ == '__main__':
+    main()
